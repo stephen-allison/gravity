058c58e1b18c93f780ecf534aa4fa1e67bd12503
Stephen Allison <git@steve.allison.clara.co.uk>
2021-01-25 20:01:53 +0000

    implemented segments-intersect? and tried out property testing

--- src/gravity/geometry.clj
+++ src/gravity/geometry.clj
@@ -1,10 +1,82 @@
 (ns gravity.geometry)
 
 
-(defn rotate [[x y] angle]
+(defn rotate 
+  "Rotates a point [x y] by given angle (in radians) clockwise about origin"
+  [[x y] angle]
   (let [x' (- (* x (Math/cos angle)) (* y (Math/sin angle)))
         y' (+ (* x (Math/sin angle)) (* y (Math/cos angle)))]
     [x' y']))
 
-(defn translate [[x y] [dx dy]]
+(defn translate 
+  "Translates a point [x y] by [dx dy]"
+  [[x y] [dx dy]]
   [(+ x dx) (+ y dy)])
+
+(defn cross-product 
+  "2D cross product of vectors [u1 v1] -> [u2 v2] and [x1 y1] -> [x2 y2]"
+  [[[u1 v1] [u2 v2]] [[x1 y1] [x2 y2]]]
+  (let [[x' y'] [(- x2 x1) (- y2 y1)]
+        [u' v'] [(- u2 u1) (- v2 v1)]]
+    (- (* u' y') (* x' v'))))
+
+(cross-product [[0 0] [1 1]] [[0 0] [3 4]])
+(* (Math/sqrt 2) 5  (Math/sin (-  (Math/atan (/ 4 3)) (/ Math/PI 4)))) 
+
+(defn direction
+  "Whether bend in line between three points is to left or right or line is straight.
+   Returns number < 0 if turn is to left
+   Returns number > 0 if turn is to right
+   Returns 0 if line is straight
+  "
+  [[x0 y0] [x1 y1] [x2 y2]]
+  (cross-product [[0 0] [(- x2 x0) (- y2 y0)]] 
+                 [[0 0] [(- x1 x0) (- y1 y0)]]))
+
+
+
+(defn on-segment 
+  "Whether point [x3 y3] lies on segment between points [x1 y1] and [x2 y2]"
+  [[x1 y1] [x2 y2] [x3 y3]]
+  (let [x-check (<= (min x1 x2) x3 (max x1 x2))
+        y-check (<= (min y1 y2) y3 (max y1 y2))]
+    (and x-check y-check)
+))
+
+(defn segments-intersect?
+  "Determine if two line segments p1->p2 and p3->p4 intersect
+   See SEGMENTS-INTERSECT, CLRS 3e p1018"
+  [[p1 p2] [p3 p4]]
+  (let [d1 (direction p3 p4 p1)
+        d2 (direction p3 p4 p2)
+        d3 (direction p1 p2 p3)
+        d4 (direction p1 p2 p4)
+        direction-check (and  (< (* d1 d2) 0) (< (* d3 d4) 0))]
+    (if direction-check
+      true
+      (cond (and (= 0 d1) (on-segment p3 p4 p1)) true 
+            (and (= 0 d2) (on-segment p3 p4 p2)) true
+            (and (= 0 d3) (on-segment p1 p2 p3)) true
+            (and (= 0 d4) (on-segment p1 p2 p4)) true
+            :else false))))
+
+(segments-intersect? [[0 -1] [0 1]] [[-1 0] [1 0]])
+(segments-intersect? [[0 0] [1 0]] [[0 0] [0 1]])
+(segments-intersect? [[1 1] [1 2]] [[1 1] [2 1]])
+(segments-intersect? [[1 1] [2 1]] [[1 1] [1 2]])
+(segments-intersect? [[0 0] [0 1]] [[1 1] [2 1]])
+(segments-intersect? [[1 2] [3 7]] [[9 12] [6 19]])
+(segments-intersect? [[0 0] [0 1]] [[0 0] [4 3]])
+
+(direction [-1 0] [1 0] [0 -1]) ; 3,4,1
+(direction [-1 0] [1 0] [0 1])  ; 3,4,2
+(direction [0 -1] [0 1] [-1 0]) ; 1,2,3
+(direction [0 -1] [0 1] [1 0])  ; 1,2,4 
+
+(direction [-1 0] [1 0] [-1 -1]) ; 3,4,1
+(direction [-1 0] [1 0] [1 -1])  ; 3,4,2
+(direction [0 -1] [0 1] [-1 0]) ; 1,2,3
+(direction [0 -1] [0 1] [1 0])  ; 1,2,4
+
+(* (Math/sin (/ Math/PI 4)) (Math/sqrt 2))
+(* 5 0.8)
--- test/gravity/geometry_test.clj
+++ test/gravity/geometry_test.clj
@@ -1,13 +1,16 @@
 (ns gravity.geometry-test
   (:require [clojure.test :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [gravity.geometry :as g]))
+
 
 (defn within [epsilon a b]
   (< (Math/abs (- a b)) epsilon))
 
 (def close-enough (partial within 0.000001))
-
-(close-enough 0.0004 0.0004001)
 
 (deftest test-rotation
   (testing "PI/2 rotation of (1,0)"
@@ -18,3 +21,78 @@
     (let [[x y] (g/rotate [1 0] Math/PI)]
       (is (close-enough x -1))
       (is (close-enough y 0)))))
+
+(deftest test-cross-product
+  (testing "Orthogonal vectors have cross product equal to product of lengths"
+    (is (= 4 (g/cross-product [[0 0] [2 0]] [[0 0] [0 2]]))))
+  (testing "Co-linear vectors have zero cross product"
+    (is (= 0 (g/cross-product [[0 0] [1 0]] [[5 0] [6 0]]))))
+  (testing "Parallel vectors have zero cross product"
+    (is (= 0 (g/cross-product [[2 2] [3 3]] [[5 5] [6 6]]))))
+  (testing "[-3,7]x[2,5] = -29"
+    (is (= -29 (g/cross-product [[0 0] [-3 7]] [[0 0] [2 5]]))))
+  (testing "[-3,7]x[2,5] = -29 after points translated"
+    (is (= -29 (g/cross-product [[1 1] [-2 8]] [[2 2] [4 7]])))))
+
+; a x b = |a||b|sinA
+(defspec test-cross-product-of-vectors-is-sine-of-angle-between-them-times-lengths 1000
+  (prop/for-all [angles (gen/vector (gen/double* {:min 0 :max (* 2 Math/PI) :NaN? false}) 2)
+                [len-1 len-2] (gen/vector (gen/double* {:min 0 :max 1000 :NaN? false}) 2)] 
+                (let [[a b] (sort angles)
+                      angle (- b a)
+                      p1 (g/rotate [len-1 0] a)
+                      p2 (g/rotate [len-2 0] b)
+                      cp (g/cross-product [[0 0] p1] [[0 0] p2])
+                      expected (* len-1 len-2 (Math/sin angle))]
+                  (close-enough expected cp))))
+
+(g/cross-product [[0 0] (g/rotate [1 0] 0.5) ] [[0 0] (g/rotate [1 0] 1.0)])
+(g/rotate [2 0] 0.5)
+
+(deftest test-direction
+  (testing "Left turn gives negative result"
+    (is (> 0 (g/direction [0 0] [1 1] [0 2]))))
+  (testing "Right turn gives positive result"
+    (is (< 0 (g/direction [0 0] [1 1] [2 0]))))
+  (testing "Co-linear points gives zero"
+    (is (= 0 (g/direction [0 0] [1 1] [2 2])))))
+
+(deftest test-segments-intersect?
+  (testing "x- and y- axes intersect"
+    (is (true? (g/segments-intersect? [[-1 0] [1 0]] [[0 -1] [0 1]]))))
+  (testing "Parallel lines don't intersect"
+    (is (false? (g/segments-intersect? [[1 1] [5 1]] [[1 2] [5 2]]))))
+  (testing "Overlapping co-linear lines intsersect"
+    (is (true? (g/segments-intersect? [[1 1] [3 3]] [[2 2] [4 4]]))))
+  (testing "Common endpoint counts as intersection"
+    (is (true? (g/segments-intersect? [[0 0] [1 1]] [[0 2] [1 1]]))))
+  (testing "Non-orthodonal intersecting lines"
+    (is (true? (g/segments-intersect? [[-1 1] [9 3]] [[-1 8] [9 -3]]))))
+  (testing "Non-orthodonal non-intersect?ing lines"
+    (is (false? (g/segments-intersect? [[-1 1] [9 3]] [[-1 8] [1 3]])))))
+
+(defspec test-overlapping-cords-intersect 1000
+  (prop/for-all [angles (gen/vector-distinct (gen/double* 
+                                              {:min 1E-5 
+                                               :max (* 2 Math/PI) 
+                                               :NaN? false })
+                                              {:num-elements 4})]
+                (let [[a b c d] (sort angles)
+                      p1 (g/rotate [10 0] a)
+                      p2 (g/rotate [10 0] b)
+                      p3 (g/rotate [10 0] c)
+                      p4 (g/rotate [10 0] d)]
+                  (is (true? (g/segments-intersect? [p1 p3] [p2 p4])) (str [a b c d] [p1 p3] [p2 p4])))))
+
+(defspec test-non-overlapping-cords--do-not-intersect 1000
+  (prop/for-all [angles (gen/vector-distinct (gen/double* 
+                                              {:min 1E-5 
+                                               :max (* 2 Math/PI) 
+                                               :NaN? false })
+                                              {:num-elements 4})]
+                (let [[a b c d] (sort angles)
+                      p1 (g/rotate [10 0] a)
+                      p2 (g/rotate [10 0] b)
+                      p3 (g/rotate [10 0] c)
+                      p4 (g/rotate [10 0] d)]
+                  (is (false? (g/segments-intersect? [p1 p2] [p3 p4])) (str [a b c d] [p1 p2] [p3 p4])))))
