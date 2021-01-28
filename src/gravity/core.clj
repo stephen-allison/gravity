280638de9cccbbf1f412f61740b74a73198b4b72
Stephen Allison <git@steve.allison.clara.co.uk>
2021-01-28 23:40:11 +0000

    identify points involved in collision

--- src/gravity/core.clj
+++ src/gravity/core.clj
@@ -4,17 +4,27 @@
             [gravity.geometry :as g]))
 
 (def gravity-strength -0.03)
-(def thrust-strength 0.1)
-(def initial-state {:ship 
-                    {:heading 0 
-                     :pos [400 300] 
-                     :v [0 0] 
-                     :forces {:gravity [0 gravity-strength]}
-                     :feelers []}
-                    :platforms [[350 50 450 50]]
-                    })    
 
-(def ship-points [-9 -6 9 -6 0 18])
+(def thrust-strength 0.1)
+
+(def initial-ship-state {:type :ship
+                         :heading 0 
+                         :pos [400 300] 
+                         :v [0 0] 
+                         :forces {:gravity [0 gravity-strength]}
+                         :feelers []
+                         :raw-points [{:name :top :point [0 18]}
+                                      {:name :bl  :point [-9 -6]}
+                                      {:name :br  :point [9 -6]}]})
+
+(def initial-level-state {:terrain [{:type :platform :points [350 50 450 50]}
+                                    {:type :platform :points [50 120 150 120]}]})
+
+(def initial-state {:ship initial-ship-state
+                    :level initial-level-state})    
+
+(defn ship-points [ship]
+  (ship :raw-points))
 
 (defn ship-mass [ship] 1)
 
@@ -24,24 +34,40 @@
   ; setup function returns initial state.
   initial-state)
 
+(defn dispatch-collision-points [item]
+  (item :type))
+
+(defmulti collision-points dispatch-collision-points)
+
+(defmethod collision-points :ship [ship]
+  (ship :feelers))
+
+(defmethod collision-points :platform [platform]
+  [{:name (platform :name) :points (partition 2 (platform :points))}])
+  
+(collision-points {:type :platform, :points [350 50 450 50]})
+
 (defn current-ship-points [ship]
-  (let [rotated (map #(g/rotate %1 (ship :heading)) (partition 2 ship-points))] 
-    (map #(g/translate %1 (ship :pos)) rotated)))
+  (let [rotated (map (fn [pt] {:name (:name pt) 
+                               :point (g/rotate (:point pt) (ship :heading))}) 
+                     (ship-points ship))] 
+    (map (fn [pt] {:name (:name pt) 
+                   :point (g/translate (:point pt) (ship :pos))}) rotated)))
  
 (defn draw-ship [ship]
-  (let [translated (current-ship-points ship)]
+  (let [translated (map :point (current-ship-points ship))]
     (q/fill 255 255 255)
     (apply q/triangle (flatten translated))
     (q/stroke 255 0 0)
     (q/stroke-weight 2)
-    (doseq [[p1 p2] (ship :feelers)]
+    (doseq [[p1 p2] (map :points (ship :feelers))]
       (q/line p1 p2))   
 ))           
 
-(defn draw-platforms [platforms]
-  (doseq [line platforms]
+(defn draw-platforms [terrain]
+  (doseq [item terrain]
     (q/stroke 255 255 255)           
-    (apply q/line line)))
+    (apply q/line (:points item))))
        
 (defn thrust-off [state]  
   (assoc-in state [:ship :forces :thrust] [0 0])) 
@@ -72,8 +98,8 @@
   (let [v (get-in state [:ship :v])
         pos (get-in state [:ship :pos])
         feelers (for [pt (current-ship-points (state :ship))] 
-                  [pt (g/translate pt v)])]
-    (assoc-in state [:ship :feelers] feelers)
+                  (assoc pt :points [(:point pt) (g/translate (:point pt) v)]))]
+    (assoc-in state [:ship :feelers] (into [] feelers))
 ))
 
 (defn update-velocity [state]
@@ -85,21 +111,25 @@
                       [vx vy] 
                       (vals forces)))))
 
+(defn collision? [item-1 item-2]
+  (let [pts1 (collision-points item-1) 
+        pts2 (collision-points item-2)
+        collisions (for [p1 pts1 p2 pts2 
+                         :when (g/segments-intersect (:points p1) (:points p2))]
+                     [(:name p1) (:name  p2)])]
+    (if (not (empty? collisions)) (println collisions) nil)))
+  
+
 (defn check-collisions [state]
-  (let [obstacles (get-in state [:platforms])
-        lines (map #(partition 2 %1) obstacles)
-        feelers (get-in state [:ship :feelers])
-        colls (for [f feelers ln lines] (g/segments-intersect? f ln))]
-    (when (some true? colls)
-      (println (str "Collision " (vector colls)))
-      state)
-    state               
-))
+  (let [terrain (get-in state [:level :terrain])
+        colls (map #(collision? (state :ship) %1) terrain)]
+    (assoc state :collisions (into [] colls))
+    ))
 
 (defn update-state [state]
   (-> state
       update-velocity
-      update-collision-feelers       
+      update-collision-feelers        
       update-position
       update-with-key-input          
       check-collisions
@@ -110,7 +140,7 @@
   (q/translate 0 -600)
   (q/background 0 0 23)
   (draw-ship (state :ship))
-  (draw-platforms (state :platforms)))
+  (draw-platforms (get-in state [:level :terrain])))
 
 (q/defsketch gravity
   :title "Gravity"
